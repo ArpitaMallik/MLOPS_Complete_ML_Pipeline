d94cd9ae597d0e7f6c953cf710d255b2c7906e63
Arpita Mallik <arpitamallik13@gmail.com>
2025-05-25 01:30:20 +0600

    logging done

---
+++ src/data_ingestion.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import logging
+
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+#creating a logger object
+logger = logging.getLogger('data_ingestion')
+logger.setLevel("DEBUG")
+
+#creating a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel("DEBUG")
+
+#creating a file handler
+log_file_path = os.path.join(log_dir, 'data_ingestion.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel("DEBUG")
+
+#creating a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_data(data_url: str) -> pd.DataFrame:
+    """Load data from a csv file."""
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug('Data loaded successfully from %s', data_url)
+        return df
+    except pd.errors.ParseError as e:
+        logger.error('Error parsing CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error while loading data: %s', e)
+        raise
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data."""
+    try:
+        df.drop(columns=['Unnamed: 2', 'Unnamed: 3', "Unnamed: 4"], inplace=True)
+        df.rename(columns={'v1': 'target', 'v2': 'text'}, inplace=True)
+        logger.debug('Data preprocessed successfully')
+        return df
+    except KeyError as e:
+        logger.error('Missing columns in the DataFrame: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error during preprocessing: %s', e)
+        raise
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    """Save the train and test data to csv files."""
+    try:
+        raw_data_path = os.path.join(data_path, 'raw')
+        os.makedirs(raw_data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path, 'train.csv'), index=False)
+        test_data.to_csv(os.path.join(raw_data_path, 'test.csv'), index=False)
+        logger.debug('Data saved successfully to %s', raw_data_path)
+    except Exception as e:
+        logger.error('Error saving data: %s', e)
+        raise
+
+def main():
+    try:
+        test_size = 0.2
+        data_path = 'https://raw.githubusercontent.com/ArpitaMallik/Datasets/refs/heads/main/spam.csv'
+        df = load_data(data_url=data_path)
+        final_df = preprocess_data(df)
+        train_data,test_data = train_test_split(final_df, test_size=test_size, random_state=2)
+        save_data(train_data,test_data,data_path='./data')
+    except Exception as e:
+        logger.error('An error occurred in the data ingestion process: %s', e)
+        print(f"An error occurred: {e}")
+        
+if __name__ == "__main__":
+    main()
+    logger.info('Data ingestion process completed successfully.')
